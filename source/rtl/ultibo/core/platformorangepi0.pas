bd4114dcfe4d8adc53244b63225b9ac2c908457a
Rob Judd <judd@ob-wan.com>
2016-11-16 14:02:28 +1000

    Add Orange Pi Zero platform

---
+++ source/rtl/ultibo/core/platformorangepi0.pas
@@ -0,0 +1,64 @@
+{
+Ultibo Platform interface unit for Orange Pi Zero
+
+Copyright (C) 2016 - Rob Judd <judd@ob-wan.com>
+
+Arch
+====
+
+ ARMv7 (Cortex A7)
+
+Boards
+======
+
+ Orange Pi Zero
+
+Licence
+=======
+
+ LGPLv2.1 with static linking exception (See COPYING.modifiedLGPL.txt)
+ 
+Credits
+=======
+
+ Information for this unit was obtained from:
+
+ 
+References
+==========
+
+
+Platform Orange Pi Zero
+=======================
+
+}
+
+{$mode delphi} {Default to Delphi compatible syntax}
+{$H+}          {Default to AnsiString}
+{$inline on}   {Allow use of Inline procedures}
+
+unit PlatformOrangePi0; 
+
+interface
+
+uses GlobalConfig,GlobalConst,GlobalTypes,Platform,PlatformARM,PlatformARMv7,HeapManager,SysUtils;
+
+{==============================================================================}
+{Global definitions}
+{$INCLUDE GlobalDefines.inc}
+
+{==============================================================================}
+
+//To Do
+
+{==============================================================================}
+{==============================================================================}
+
+implementation
+
+{==============================================================================}
+
+
+{==============================================================================}
+
+end.
