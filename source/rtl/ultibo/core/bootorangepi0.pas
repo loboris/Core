d4a595bed7e6f61daa7100dc0f36ad1b9786d2af
Rob Judd <judd@ob-wan.com>
2016-11-23 20:24:45 +1000

    Update board specifications

--- source/rtl/ultibo/core/bootorangepi0.pas
+++ source/rtl/ultibo/core/bootorangepi0.pas
@@ -35,33 +35,34 @@ Orange Pi Zero
  
  Model: Orange Pi Zero
  
- Stock OS: Lubuntu 14.04
+ Stock OS: Lubuntu 14.04 (also supports Android, Debian)
 
  SoC: Allwinner H2
  
- CPU: ARMv7-A Cortex-A7 (4 @ ??GHz)
+ CPU: ARMv7-A Cortex-A7 (4 @ 1.2GHz)
  
  Cache: L1 ??KB I/D, L2 ???KB (Shared)
  
- GPU: Mali-400 (? pixel processors + ? vertex shaders) OpenGL ES2 ???MHz + AVE H264/H265/VC1
+ GPU: Mali-400MP2 @600MHz (? pixel processors + ? vertex shaders) OpenGL ES2 + AVE H264/H265/VC1
  
  FPU: ???
  
- RAM: 256MB or 512MB (Type?)
+ RAM: 256MB (??) or 512MB (SEC628 K484G16)
  
- USB: 1 USB 2.0 host + 2 USB (on 13-pin header) + Micro-B OTG 
+ USB: 1 USB 2.0 host + 2 USB (13-pin header) + Micro-B OTG 
  
- LAN: 10/100
+ LAN: 10/100 passive jack with H1102NL external magnetics and POE
   
- SD/MMC: MicroSD Card slot, optional SPI NOR flash on board
+ SD/MMC: MicroSD Card slot
  
- WiFi: Allwinner XR819
+ WiFi: Allwinner XR819 (RTL8189) 802.11 b/g/n and mini wifi antenna
  
  Bluetooth: Nil
  
  Display: CVBS (on 13-pin header)
  
- Other: GPIO (26), IR (on 13-pin header)
+ Other: RasPi B+ compatible GPIO (26-pin header), IR + TVOUT + Mic + Stereo Audio + 2 USB (13-pin header)
+ Other: Serial debug port header, optional 2MB SPI NOR flash
 
 }
 
